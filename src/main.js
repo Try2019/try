466fafe844c2a41d477a1ced27da33669e210183
Try2019 <wzq295386498@outlook.com>
2019-09-10 11:29:56 +0800

    after swipe

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 //入口文件
-import Vue from 'vue/dist/vue'
+import Vue from 'vue'
 import myRouter from './router.js'
 import app from './app.vue'
 
@@ -7,7 +7,14 @@ import {Header} from 'mint-ui';
 import 'mint-ui/lib/style.css'
 Vue.component(Header.name,Header);
 
+//轮播图的使用
+import { Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//购物车图标的使用
 import './lib/dist/css/mui.min.css'
+import './lib/dist/css/icons-extra.css'
 
 let vm=new Vue({
   el:'#app',
@@ -19,5 +26,6 @@ let vm=new Vue({
   },
   render:function (createElement) {
     return createElement(app);
-  }
+  },
+  router:myRouter
 });
--- src/router.js
+++ src/router.js
@@ -1,10 +1,22 @@
 import VueRouter from 'vue-router';
+import Vue from 'vue'
 import app from './app.vue'
+import home from './components/home.vue'
+import search from './components/search.vue'
+import shopping from './components/shopping.vue'
+import user from './components/user.vue'
+Vue.use(VueRouter);
+
 
 let myRouter=new VueRouter({
   routes:[
-  
-  ]
+    {path:'/',component:app},
+    {path:'/home',component:home},
+    {path:'/user',component:user},
+    {path:'/search',component:search},
+    {path:'/shopping',component:shopping}
+  ],
+  linkActiveClass:'mui-active'
 })
 
 export default myRouter;
