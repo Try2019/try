497d06ce3ef46281d39db00373f8d40117157b8f
Try2019 <wzq295386498@outlook.com>
2019-09-11 15:54:21 +0800

    second day

---
+++ src/components/subComponents/newsList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-right" src="../../imgs/yuantiao.jpg">
+          <div class="mui-media-body">
+            <h2>远眺</h2>
+            <p class='mui-ellipsis'><span>Time:2019/09/11</span><span>click:0</span></p>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-right" src="../../imgs/shuijiao.jpg">
+          <div class="mui-media-body">
+            <h2>幸福</h2>
+            <p class='mui-ellipsis'><span>Time:2019/09/11</span><span>click:0</span></p>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-right" src="../../imgs/muwu.jpg">
+          <div class="mui-media-body">
+            <h2>木屋</h2>
+            <p class='mui-ellipsis'><span>Time:2019/09/11</span><span>click:0</span></p>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .mui-table-view h2{
+    font-size:16px;
+  }
+  .mui-ellipsis{
+    color: #4da8ff;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -5,16 +5,18 @@ import home from './components/home.vue'
 import search from './components/search.vue'
 import shopping from './components/shopping.vue'
 import user from './components/user.vue'
+import newsList from './components/subComponents/newsList.vue'
 Vue.use(VueRouter);
 
 
 let myRouter=new VueRouter({
   routes:[
     {path:'/',redirect:'/home'},
-    {path:'/home',component:home},
+    {path:'/home', component:home},
     {path:'/user',component:user},
     {path:'/search',component:search},
-    {path:'/shopping',component:shopping}
+    {path:'/shopping',component:shopping},
+    {path:'/home/news',component:newsList}
   ],
   linkActiveClass:'mui-active'
 })
